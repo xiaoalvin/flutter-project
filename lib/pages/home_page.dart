80a8e68bb299e112f36d8b940569dc75006d0693
Alvin <alvin.n@swzph.com>
2020-03-22 15:10:07 +0800

    fix#dio_request 尝试dio

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,12 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:dio/dio.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    getHttp();
     return Scaffold(
       body: Center(
         child: Text('商城首页'),
       )
     );
   }
+
+
+  void getHttp() async{
+    try{
+      Response response;
+      response = await Dio().get('http://yapi.demo.qunar.com/mock/90770/api/v1/test');
+      return print(response.data);
+    }catch(error) {
+      return print(error);
+    }
+  }
+
 }
