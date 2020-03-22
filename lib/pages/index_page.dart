00440293e5020e0a13837694c1c61f4ad090d2f8
Alvin <alvin.n@swzph.com>
2020-03-22 14:11:15 +0800

    fix#bofix#bottom_bar 制作bottom bar

--- lib/pages/index_page.dart
+++ lib/pages/index_page.dart
@@ -1,15 +1,69 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import './home_page.dart';
+import './category_page.dart';
+import './cart_page.dart';
+import './member_page.dart';
+class IndexPage extends StatefulWidget {
+  @override
+  _IndexPageState createState() => _IndexPageState();
+}
 
-class IndexPage extends StatelessWidget {
+class _IndexPageState extends State<IndexPage> {
+  // 底部bar的list
+  final List<BottomNavigationBarItem> bottomTabs = [
+    BottomNavigationBarItem(
+      icon: Icon(CupertinoIcons.home),
+      title: Text('首页')
+    ),
+    BottomNavigationBarItem(
+      icon: Icon(CupertinoIcons.search),
+      title: Text('分类')
+    ),
+    BottomNavigationBarItem(
+      icon: Icon(CupertinoIcons.shopping_cart),
+      title: Text('购物车')
+    ),
+    BottomNavigationBarItem(
+      icon: Icon(CupertinoIcons.profile_circled),
+      title: Text('会员中心')
+    ),
+  ];
+  // 对应的页面
+  final List tabBodies = [
+    HomePage(),
+    CategoryPage(),
+    CartPage(),
+    MemberPage(),
+  ];
+
+  int currentIndex = 0; // 当前索引
+  var currentPage;
+
+  @override
+  void initState() {
+    // 初始化
+    currentPage = tabBodies[currentIndex]; // 打开时，默认的
+    super.initState();
+
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('百姓生活+')
+      backgroundColor: Color.fromRGBO(244, 245, 245, 1),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed, // 类型？，固定
+        currentIndex: currentIndex, // 当前页面
+        items: bottomTabs,
+        onTap: (index){
+          setState(() {
+            currentIndex = index;
+            currentPage = tabBodies[currentIndex];
+          });
+        },
       ),
-      body: Center(
-        child: Text('百姓生活+'),
-      )
+      body: currentPage,
     );
   }
 }
